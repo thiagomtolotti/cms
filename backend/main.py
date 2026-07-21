ce05cea31b98d6f3f42fe9ce65f97f0848110774
Thiago Tolotti <thiagotolotti@gmail.com>
2026-07-21 09:59:28 -0300

    adds GET blog post request

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 def ping():
     return {"message": "CMS service is alive!"}
+
+
+@app.get("/{post_slug}")
+def get_post(post_slug: str):
+    print(f"Received request for post: {post_slug}")
+
+    return {"message": f"Request received for post: {post_slug}"}

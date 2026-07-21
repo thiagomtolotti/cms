aeaa22ab278ae69031f17bb798985c1cfff53a38
Thiago Tolotti <thiagotolotti@gmail.com>
2026-07-21 10:05:38 -0300

    transforms markdown into html responses

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
+import markdown
+
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -8,8 +12,13 @@ def ping():
     return {"message": "CMS service is alive!"}
 
 
-@app.get("/{post_slug}")
+@app.get("/{post_slug}", response_class=HTMLResponse)
 def get_post(post_slug: str):
-    print(f"Received request for post: {post_slug}")
+    file_path = Path(__file__).parent / "data" / f"{post_slug}.md"
 
-    return {"message": f"Request received for post: {post_slug}"}
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    html = markdown.markdown(content)
+
+    return html

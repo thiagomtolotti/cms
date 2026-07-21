432d7071ddfdefab3e65d47aef063c29c667a4cd
Thiago Tolotti <thiagotolott@gmail.com>
2026-07-21 11:20:27 -0300

    adds blog post entity with repository and inmemory implementation

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,15 @@
+from typing import Any
+
 import markdown
 
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+
+from src.exceptions import EntityNotFoundError
+from src.infra.post.inmemory import InMemoryPostRepository
+from src.domain.post_repository import PostRepository
 
 app = FastAPI()
 
@@ -24,13 +30,14 @@ def ping():
 ROOT_PATH = Path(__file__).parent.parent
 DATA_PATH = ROOT_PATH / "data"
 
+repo: PostRepository = InMemoryPostRepository()
+
 
 @app.get("/{post_slug}", response_class=HTMLResponse)
 def get_post(post_slug: str):
-    file_path = DATA_PATH / f"{post_slug}.md"
+    post = repo.get_from_slug(post_slug)
 
-    if not file_path.exists():
-        return HTMLResponse(content="<h1>404 Not Found</h1>", status_code=404)
+    file_path = DATA_PATH / post.file
 
     with open(file_path, "r") as file:
         content = file.read()
@@ -38,3 +45,16 @@ def get_post(post_slug: str):
     html = markdown.markdown(content)
 
     return html
+
+
+def entity_not_found_error_handler(_: Any, exc: Exception):
+    return HTMLResponse(
+        content="<h1>404 Not Found</h1>",
+        status_code=404,
+    )
+
+
+app.add_exception_handler(
+    EntityNotFoundError,
+    entity_not_found_error_handler,
+)

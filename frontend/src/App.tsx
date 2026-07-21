eb15865a2cea4a05f335d63e94ca0d6d96b80000
Thiago Tolotti <thiagotolott@gmail.com>
2026-07-21 14:28:56 -0300

    adds create post page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,36 @@
 import BlogPostContainer from "./contexts/blog-post/components/content";
+import CreatePostForm from "./contexts/blog-post/components/create-post-form";
 
 function App() {
-  return <BlogPage />;
-}
-
-function BlogPage() {
   const slug = window.location.pathname.split("/").pop();
 
   if (!slug) {
     return <h1>No slug provided</h1>;
   }
 
+  if (slug === "create") {
+    return <CreatePostPage />;
+  }
+
+  return <BlogPage slug={slug} />;
+}
+
+interface BlogPageProps {
+  slug: string;
+}
+
+function BlogPage({ slug }: BlogPageProps) {
   return <BlogPostContainer slug={slug} />;
+}
+
+function CreatePostPage() {
+  return (
+    <section className="flex flex-col gap-8 max-w-4xl mx-auto my-16">
+      <h1 className="w-full mb-8">Crie um novo post</h1>
+
+      <CreatePostForm />
+    </section>
+  );
 }
 
 export default App;

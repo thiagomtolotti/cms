ea3e0b26f5c8aa1caa5822e1db9052c335900539
Thiago Tolotti <thiagotolotti@gmail.com>
2026-07-21 12:54:03 -0300

    applies tailwind in blog post content css

--- frontend/src/contexts/blog-post/components/content.tsx
+++ frontend/src/contexts/blog-post/components/content.tsx
@@ -1,17 +1,18 @@
-import "./post.css";
+import { useEffect } from "react";
 
 import useBlogPost from "../hooks/useBlogPost";
 import usePostMetadata from "../hooks/usePostMetadata";
-import { useEffect } from "react";
-import getPostImage from "../lib/fetchPostImage";
 
-interface BlogPostContentProps {
+import getPostImage from "../lib/fetchPostImage";
+import type { PostMetadata } from "../lib/fetchMetadata";
+
+interface BlogPostContainerProps {
   slug: string;
 }
 
-export default function BlogPostContent({ slug }: BlogPostContentProps) {
+export default function BlogPostContainer({ slug }: BlogPostContainerProps) {
   const { content, isPending, error } = useBlogPost(slug);
-  const { metadata } = usePostMetadata(slug);
+  const { metadata, isPending: isMetadataPending } = usePostMetadata(slug);
 
   useEffect(() => {
     const title = window.document.querySelector("title");
@@ -27,29 +28,45 @@ export default function BlogPostContent({ slug }: BlogPostContentProps) {
   }, [metadata?.title]);
 
   return (
-    <section className="blog-post">
-      {isPending && <p>Loading...</p>}
+    <section className="flex flex-col w-full max-w-5xl mx-auto my-16">
+      {(isPending || isMetadataPending) && <p>Loading...</p>}
 
       {error && (
         <div className="error" dangerouslySetInnerHTML={{ __html: error }} />
       )}
 
-      <img className="image" src={getPostImage(slug)} />
-
-      {metadata && (
-        <TitleArea
-          title={metadata.title}
-          author={metadata.author}
-          date={metadata.date}
-        />
+      {content && metadata && (
+        <BlogPostContent slug={slug} content={content} metadata={metadata} />
       )}
+    </section>
+  );
+}
 
-      {content && (
-        <div
-          className="content"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-      )}
+interface BlogPostContentProps {
+  slug: string;
+  content: string;
+  metadata: PostMetadata;
+}
+
+function BlogPostContent({ slug, content, metadata }: BlogPostContentProps) {
+  return (
+    <section className="flex flex-col gap-4">
+      <img
+        className="max-h-70 object-cover object-center mb-8"
+        src={getPostImage(slug)}
+      />
+
+      <div className="w-full max-w-3xl mx-auto flex flex-col gap-4">
+        {metadata && (
+          <TitleArea
+            title={metadata.title}
+            author={metadata.author}
+            date={metadata.date}
+          />
+        )}
+
+        {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
+      </div>
     </section>
   );
 }
@@ -62,9 +79,9 @@ interface TitleAreaProps {
 
 function TitleArea({ title, author, date }: TitleAreaProps) {
   return (
-    <div className="title">
+    <div className="flex flex-col gap-4 mb-4">
       <h1>{title}</h1>
-      <span className="metadata">
+      <span>
         {new Date(date).toLocaleDateString("pt-BR")} - {author}
       </span>
     </div>

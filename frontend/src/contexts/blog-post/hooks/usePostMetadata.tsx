4b92cf06491df2e21490d1b41a866601e5794102
Thiago Tolotti <thiagotolotti@gmail.com>
2026-07-22 16:22:21 -0300

    uses tanstack router loader

--- frontend/src/contexts/blog-post/hooks/usePostMetadata.tsx
+++
@@ -1,9 +0,0 @@
-import fetchMetadata from "../lib/fetchMetadata";
-import { useQuery } from "@tanstack/react-query";
-
-export default function usePostMetadata(slug: string) {
-  return useQuery({
-    queryKey: ["postMetadata", slug],
-    queryFn: async () => await fetchMetadata(slug),
-  });
-}
--- frontend/src/routes/$slug.tsx
+++ frontend/src/routes/$slug.tsx
@@ -1,10 +1,39 @@
-import { createFileRoute } from "@tanstack/react-router";
-import BlogPostContainer from "../contexts/blog-post/components/content";
+import type { Post } from "../contexts/blog-post/types/post";
+
+import fetchPost from "../contexts/blog-post/lib/fetchPost";
+
+import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$slug")({
   component: RouteComponent,
+  loader: async ({ params }): Promise<Post> => fetchPost(params.slug),
+  pendingComponent: () => <p>Loading...</p>,
+  errorComponent: () => <p>Houve um erro ao carregar o post</p>,
 });
 
 function RouteComponent() {
-  return <BlogPostContainer slug={Route.useParams().slug} />;
+  const post: Post = useLoaderData({ from: Route.id });
+
+  return (
+    <section className="flex flex-col gap-4">
+      <img
+        className="max-h-70 object-cover object-center mb-8"
+        src={post.imageUrl}
+      />
+
+      <div className="w-full max-w-3xl mx-auto flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mb-4">
+          <h1>{post.title}</h1>
+          <span>
+            {post.date.toLocaleDateString("pt-BR")} - {post.author}
+          </span>
+        </div>
+
+        <div
+          className="flex flex-col gap-4"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
+    </section>
+  );
 }

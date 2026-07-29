5fe4324a03803285a982465a5d1ce9d14a8876a3
Thiago Tolotti <thiagotolotti@gmail.com>
2026-07-29 14:32:52 -0300

    adds login button component

---
+++ frontend/src/components/auth/components/login-button.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { useKeycloak } from "@react-keycloak/web";
+
+import { UserRound } from "lucide-react";
+
+import type { ButtonProps } from "@base-ui/react/button";
+
+export default function LoginButton() {
+  const { keycloak, initialized } = useKeycloak();
+
+  type AuthButtonVariants = "login" | "logout" | "loading";
+
+  const variant: AuthButtonVariants = (() => {
+    if (!initialized) return "loading";
+
+    if (keycloak?.authenticated) return "logout";
+
+    return "login";
+  })();
+
+  const buttonProps: Record<AuthButtonVariants, Partial<ButtonProps>> = {
+    login: LoginButton.Login({ onClick: () => keycloak?.login() }),
+    logout: LoginButton.Logout({ onClick: () => keycloak?.logout() }),
+    loading: LoginButton.Loading({ disabled: true }),
+  };
+
+  return <Button variant="ghost" {...buttonProps[variant]} />;
+}
+
+LoginButton.Login = (props: Partial<ButtonProps>): Partial<ButtonProps> => ({
+  children: (
+    <>
+      <UserRound />
+      Login
+    </>
+  ),
+  ...props,
+});
+
+LoginButton.Logout = (props: Partial<ButtonProps>): Partial<ButtonProps> => ({
+  children: (
+    <>
+      <UserRound />
+      Logout
+    </>
+  ),
+  ...props,
+});
+
+LoginButton.Loading = (props: Partial<ButtonProps>): Partial<ButtonProps> => ({
+  children: (
+    <>
+      <UserRound />
+      Carregando...
+    </>
+  ),
+  ...props,
+});

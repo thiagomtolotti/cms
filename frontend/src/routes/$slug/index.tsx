c80c45e7d679153caf1a4cb46b22c5dcad1a8c4c
Thiago Tolotti <thiagotolotti@gmail.com>
2026-08-08 14:27:21 -0300

    chore: post image UI responsive

--- frontend/src/routes/$slug/index.tsx
+++ frontend/src/routes/$slug/index.tsx
@@ -5,6 +5,7 @@ import fetchPost from "@/components/blog-post/lib/fetchPost";
 import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 import { useKeycloak } from "@react-keycloak/web";
 import PostStatusBadge from "@/components/blog-post/components/post-status-badge";
+import { cn } from "@/lib/utils";
 
 export const Route = createFileRoute("/$slug/")({
   component: RouteComponent,
@@ -23,27 +24,30 @@ function RouteComponent() {
   const post: Post = useLoaderData({ from: Route.id });
 
   return (
-    <section className="flex flex-col gap-4">
-      <img
-        className="max-h-100 object-cover object-center mb-8 max-w-3xl w-full mx-auto rounded-2xl shadow-md "
-        src={post.imageUrl}
-      />
-
-      <div className="w-full max-w-3xl mx-auto flex flex-col gap-4 mb-16 max-lg:px-8">
-        <div className="flex flex-col gap-4 mb-4">
-          <h1>{post.title}</h1>
-
-          <span className="flex items-center gap-4">
-            {post.date.toLocaleDateString("pt-BR")} - {post.author}
-            {keycloak.authenticated && <PostStatusBadge status={post.status} />}
-          </span>
-        </div>
-
-        <div
-          className="flex flex-col gap-4"
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        />
+    <section className="flex flex-col gap-4 w-full max-w-3xl mb-16 max-lg:px-8 mx-auto">
+      <div
+        className={cn(
+          "mb-8 max-h-100 max-lg:max-h-[40dvw] max-w-3xl w-full mx-auto",
+          "rounded-2xl overflow-hidden shadow-md max-lg:rounded-lg",
+          "flex items-center justify-center",
+        )}
+      >
+        <img className="object-cover object-center" src={post.imageUrl} />
       </div>
+
+      <div className="flex flex-col gap-4 mb-4">
+        <h1>{post.title}</h1>
+
+        <span className="flex items-center gap-4">
+          {post.date.toLocaleDateString("pt-BR")} - {post.author}
+          {keycloak.authenticated && <PostStatusBadge status={post.status} />}
+        </span>
+      </div>
+
+      <div
+        className="flex flex-col gap-4"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
     </section>
   );
 }

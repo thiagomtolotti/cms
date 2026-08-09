fdaea00738dba7fc3eb9826322358dec5555ec8c
Thiago Tolotti <thiagotolotti@gmail.com>
2026-08-09 16:22:09 -0300

    transaction list in own file

---
+++ frontend/src/components/finances/components/transaction_list.tsx
@@ -0,0 +1,44 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function TransactionList() {
+  return (
+    <div className="col-span-full">
+      <Table>
+        <TableCaption></TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Descrição</TableHead>
+            <TableHead>Valor</TableHead>
+            <TableHead>Data</TableHead>
+            <TableHead>Tipo</TableHead>
+            <TableHead>Ações</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Mercado</TableCell>
+            <TableCell>R$ 100,00</TableCell>
+            <TableCell>{new Date().toLocaleDateString("pt-BR")}</TableCell>
+            <TableCell>Despesa</TableCell>
+            <TableCell>
+              <button className="text-blue-500 hover:underline">Editar</button>
+              <button className="text-red-500 hover:underline ml-2">
+                Excluir
+              </button>
+            </TableCell>
+          </TableRow>
+
+          <TableRow />
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- frontend/src/routes/financas/index.tsx
+++ frontend/src/routes/financas/index.tsx
@@ -1,14 +1,8 @@
-import ProtectedRoute from "@/components/auth/components/protected-route";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { createFileRoute } from "@tanstack/react-router";
+
+import ProtectedRoute from "@/components/auth/components/protected-route";
+
+import TransactionList from "@/components/finances/components/transaction_list";
 
 export const Route = createFileRoute("/financas/")({
   component: RouteComponent,
@@ -21,40 +15,5 @@ function RouteComponent() {
         <TransactionList />
       </main>
     </ProtectedRoute>
-  );
-}
-
-function TransactionList() {
-  return (
-    <div className="col-span-full">
-      <Table>
-        <TableCaption></TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Descrição</TableHead>
-            <TableHead>Valor</TableHead>
-            <TableHead>Data</TableHead>
-            <TableHead>Tipo</TableHead>
-            <TableHead>Ações</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell>Mercado</TableCell>
-            <TableCell>R$ 100,00</TableCell>
-            <TableCell>{new Date().toLocaleDateString("pt-BR")}</TableCell>
-            <TableCell>Despesa</TableCell>
-            <TableCell>
-              <button className="text-blue-500 hover:underline">Editar</button>
-              <button className="text-red-500 hover:underline ml-2">
-                Excluir
-              </button>
-            </TableCell>
-          </TableRow>
-
-          <TableRow />
-        </TableBody>
-      </Table>
-    </div>
   );
 }

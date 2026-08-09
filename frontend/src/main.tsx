0e8a7292d4ed197f54c346dbcd1a828f4591dbae
Thiago Tolotti <thiagotolotti@gmail.com>
2026-08-09 12:12:59 -0300

    fix: handles initialization error by preventing load until keycloak is initialized

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { routeTree } from "./routeTree.gen";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import { ReactKeycloakProvider } from "@react-keycloak/web";
+import { ReactKeycloakProvider, useKeycloak } from "@react-keycloak/web";
 
 import keycloak from "./lib/keycloak";
 import queryClient from "./lib/queryClient";
@@ -29,9 +29,21 @@ createRoot(document.getElementById("root")!).render(
         }
       }}
     >
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <WithKeycloakProviderContent />
     </ReactKeycloakProvider>
   </StrictMode>,
 );
+
+function WithKeycloakProviderContent() {
+  const { initialized } = useKeycloak();
+
+  if (!initialized) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
